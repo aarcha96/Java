5eb41d05c9a666ceb823df159d6a7fa05b18b71b
aarcha96 <aarchas1996@gmail.com>
2025-02-01 08:05:07 +0530

    Boxing Unboxing example

---
+++ JavaLearning/src/com/learn/java/numericstreams/NumericStreamBoxingUnboxingExample.java
@@ -0,0 +1,36 @@
+package com.learn.java.numericstreams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamBoxingUnboxingExample {
+
+    public static void main(String[] args) {
+        List<Integer> l = boxing();
+        System.out.println(l);
+        System.out.println(unboxing(l));
+
+
+    }
+
+    public static List<Integer> boxing() {
+        //boxing
+        //convert primitive to wrapper class type
+        //int to Integer
+        //this will convert intstream to wrapper Integer List
+        return IntStream.rangeClosed(1,20)
+                .boxed() //this will convert intstream to wrapper Integer List
+                .toList();
+    }
+
+    public static int unboxing(List<Integer> list){
+        //unboxing
+        //convert wrapper class type to primitive
+        //Integer to int
+        return list.stream()
+                .mapToInt(Integer::intValue) //mapToInt convert to IntStream
+                .sum();
+    }
+}
